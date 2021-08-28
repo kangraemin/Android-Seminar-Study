55e5350d6a910957fec1ded32c3766ab6141a525
anjahyun <goleftleft@gmail.com>
2021-08-28 21:26:35 +0900

    [추가]배달의민족 카피 앱 프로젝트 생성

---
+++ Jaehyun/app/src/main/java/android/anjahyun/study/MainActivity.kt
@@ -0,0 +1,11 @@
+package android.anjahyun.study
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

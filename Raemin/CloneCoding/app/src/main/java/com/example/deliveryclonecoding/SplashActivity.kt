6bd6944585eabb19b400704425f54b8b10b15e96
kangraemin <remin1994@gmail.com>
2021-08-29 19:57:08 +0900

    [Add] Splash UI

---
+++ Raemin/CloneCoding/app/src/main/java/com/example/deliveryclonecoding/SplashActivity.kt
@@ -0,0 +1,31 @@
+package com.example.deliveryclonecoding
+
+import android.os.Bundle
+import android.text.SpannableString
+import android.text.Spanned.SPAN_INCLUSIVE_INCLUSIVE
+import android.text.style.AbsoluteSizeSpan
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.deliveryclonecoding.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setSplashTitle()
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_splash)
+    }
+
+    private fun setSplashTitle() {
+        binding.tvSplashTitle.text =
+            SpannableString(resources.getString(R.string.splash_title)).apply {
+                setSpan(
+                    AbsoluteSizeSpan(
+                        resources.getDimension(R.dimen.splash_title_particle_size).toInt()
+                    ), 2, 3, SPAN_INCLUSIVE_INCLUSIVE
+                )
+            }
+    }
+}

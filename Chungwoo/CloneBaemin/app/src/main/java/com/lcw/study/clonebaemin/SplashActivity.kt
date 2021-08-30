3ff1fa3cc9fee9d2aa8ff20ac6b43447878f5384
LeeChungWoo <skcjddn@gmail.com>
2021-08-31 08:54:11 +0900

    Add splash move timer

--- Chungwoo/CloneBaemin/app/src/main/java/com/lcw/study/clonebaemin/SplashActivity.kt
+++ Chungwoo/CloneBaemin/app/src/main/java/com/lcw/study/clonebaemin/SplashActivity.kt
@@ -1,11 +1,28 @@
 package com.lcw.study.clonebaemin
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import java.util.*
+import kotlin.concurrent.timer
 
 class SplashActivity : AppCompatActivity() {
+    private var timerTask: Timer? = null      // null을 허용
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        timerTask = timer(period = 1000, initialDelay = 3000) {
+
+            runOnUiThread {
+                val intent = Intent(this@SplashActivity, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+            timerTask?.cancel()
+
+        }
+
+
     }
 }

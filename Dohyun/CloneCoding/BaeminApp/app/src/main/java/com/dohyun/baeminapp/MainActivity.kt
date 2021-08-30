57e0062299d12cdf1d258a92c7efe5fd00fc8e28
xxria17 <xxrcy32@gmail.com>
2021-08-30 16:07:53 +0900

    Set primary fragment in MainActivity

--- Dohyun/CloneCoding/BaeminApp/app/src/main/java/com/dohyun/baeminapp/MainActivity.kt
+++ Dohyun/CloneCoding/BaeminApp/app/src/main/java/com/dohyun/baeminapp/MainActivity.kt
@@ -2,11 +2,19 @@ package com.dohyun.baeminapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.commitNow
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.SplashTheme)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager.commitNow(true) {
+            val mainFragment : Fragment = MainFragment()
+            replace(R.id.main_content, mainFragment)
+            setPrimaryNavigationFragment(mainFragment)
+        }
     }
 }

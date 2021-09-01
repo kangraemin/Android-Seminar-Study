4988b10800652f7cae26a92f7b55eb322f495830
kangraemin <remin1994@gmail.com>
2021-09-02 01:03:19 +0900

    [Add] Detail fragment for bottom nav

---
+++ Raemin/CloneCoding/app/src/main/java/com/example/deliveryclonecoding/SearchFragment.kt
@@ -0,0 +1,27 @@
+package com.example.deliveryclonecoding
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.deliveryclonecoding.databinding.FragmentSearchBinding
+
+class SearchFragment : Fragment() {
+
+    private var _binding: FragmentSearchBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentSearchBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
+    }
+}

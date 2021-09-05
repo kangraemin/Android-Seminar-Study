f9871e227cb944ad41e23e5f241cea277c9f5194
xxria17 <xxrcy32@gmail.com>
2021-09-05 21:36:17 +0900

    기존 Activity에 Base Activity 상속

--- Dohyun/CloneCoding/BaeminApp/app/src/main/java/com/dohyun/baeminapp/view/MainActivity.kt
+++ Dohyun/CloneCoding/BaeminApp/app/src/main/java/com/dohyun/baeminapp/view/MainActivity.kt
@@ -1,19 +1,17 @@
 package com.dohyun.baeminapp.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import androidx.fragment.app.commitNow
 import com.dohyun.baeminapp.R
+import com.dohyun.baeminapp.base.BaseActivity
+import com.dohyun.baeminapp.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
-        setTheme(R.style.SplashTheme)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
         supportFragmentManager.commitNow(true) {
-            val mainFragment : Fragment = MainFragment()
+            val mainFragment = MainFragment()
             replace(R.id.main_content, mainFragment)
             setPrimaryNavigationFragment(mainFragment)
         }

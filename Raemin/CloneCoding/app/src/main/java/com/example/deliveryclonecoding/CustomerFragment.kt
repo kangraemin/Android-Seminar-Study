35b8ef339b32cde862c63ccd80796014338caae8
kangraemin <remin1994@gmail.com>
2021-09-07 23:23:10 +0900

    [Add] Settings for Toolbar in custom UI

--- Raemin/CloneCoding/app/src/main/java/com/example/deliveryclonecoding/CustomerFragment.kt
+++ Raemin/CloneCoding/app/src/main/java/com/example/deliveryclonecoding/CustomerFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.findNavController
 import com.example.deliveryclonecoding.databinding.FragmentCustomerBinding
 
 class CustomerFragment : Fragment() {
@@ -17,11 +19,22 @@ class CustomerFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentCustomerBinding.inflate(inflater, container, false)
+        setToolbarSettings()
         return binding.root
     }
 
     override fun onDestroyView() {
         _binding = null
         super.onDestroyView()
+    }
+
+    private fun setToolbarSettings() {
+        setHasOptionsMenu(true)
+        (requireActivity() as AppCompatActivity).setSupportActionBar(binding.toolbar.toolbar)
+        (requireActivity() as AppCompatActivity).supportActionBar?.setDisplayShowTitleEnabled(false)
+        binding.toolbar.toolbar.setNavigationOnClickListener {
+            findNavController().navigateUp()
+        }
+        binding.toolbar.tvTitle.text = "My배민"
     }
 }

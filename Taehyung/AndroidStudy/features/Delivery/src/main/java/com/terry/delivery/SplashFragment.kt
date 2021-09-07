c433f49a62327a1deb1f331643a789ed38ea4942
Terry <choir0118@gmail.com>
2021-09-07 20:08:50 +0900

    feat : Splash Fragment 추가

---
+++ Taehyung/AndroidStudy/features/Delivery/src/main/java/com/terry/delivery/SplashFragment.kt
@@ -0,0 +1,13 @@
+package com.terry.delivery
+
+import com.terry.delivery.base.BaseFragment
+import com.terry.delivery.databinding.FragmentSplashBinding
+
+/*
+ * Created by Taehyung Kim on 2021-09-07
+ */
+class SplashFragment : BaseFragment<FragmentSplashBinding>(FragmentSplashBinding::inflate) {
+
+
+
+}

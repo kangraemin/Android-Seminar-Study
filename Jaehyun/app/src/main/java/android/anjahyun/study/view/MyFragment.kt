41e0bb3507327e2eac9d44cf574fb4330d138835
anjahyun <goleftleft@gmail.com>
2021-09-09 20:21:10 +0900

    [수정]My배민 -> 로그인페이지 연결

--- Jaehyun/app/src/main/java/android/anjahyun/study/view/MyFragment.kt
+++ Jaehyun/app/src/main/java/android/anjahyun/study/view/MyFragment.kt
@@ -1,14 +1,26 @@
 package android.anjahyun.study.view
 
+import android.anjahyun.study.R
 import android.anjahyun.study.base.BaseFragment
 import android.anjahyun.study.databinding.FragmentMyBinding
 import android.os.Bundle
 import android.view.View
+import androidx.navigation.fragment.findNavController
 
-class MyFragment: BaseFragment<FragmentMyBinding>(FragmentMyBinding::inflate) {
+class MyFragment: BaseFragment<FragmentMyBinding>(FragmentMyBinding::inflate), View.OnClickListener {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.layoutMember.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when(v) {
+            binding.layoutMember -> {
+                findNavController().navigate(R.id.loginFragment)
+            }
+        }
     }
 
 }

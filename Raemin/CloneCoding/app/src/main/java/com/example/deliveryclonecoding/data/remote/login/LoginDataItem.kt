270ed983cab0734c5fc5b9d1ceb4331041f2a3bd
kangraemin <remin1994@gmail.com>
2021-09-09 20:44:44 +0900

    [Add] Logic to get token using login api from server
    - Add retrofit client object

---
+++ Raemin/CloneCoding/app/src/main/java/com/example/deliveryclonecoding/data/remote/login/LoginDataItem.kt
@@ -0,0 +1,6 @@
+package com.example.deliveryclonecoding.data.remote.login
+
+data class LoginDataItem(
+    val access: String,
+    val refresh: String
+)
---
+++ Raemin/CloneCoding/app/src/main/java/com/example/deliveryclonecoding/data/remote/login/LoginInfo.kt
@@ -0,0 +1,6 @@
+package com.example.deliveryclonecoding.data.remote.login
+
+data class LoginInfo(
+    val username: String,
+    val password: String
+)

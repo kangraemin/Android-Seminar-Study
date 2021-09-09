08b5e4d8deec02b82d7e2a20f5d73ce1f3e4a507
xxria17 <xxrcy32@gmail.com>
2021-09-09 19:15:57 +0900

    로그인 이벤트 구현

--- Dohyun/CloneCoding/BaeminApp/app/src/main/java/com/dohyun/baeminapp/view/login/LoginFragment.kt
+++ Dohyun/CloneCoding/BaeminApp/app/src/main/java/com/dohyun/baeminapp/view/login/LoginFragment.kt
@@ -2,11 +2,16 @@ package com.dohyun.baeminapp.view.login
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
 import com.dohyun.baeminapp.R
 import com.dohyun.baeminapp.base.BaseFragment
 import com.dohyun.baeminapp.databinding.FragmentLoginBinding
 
 class LoginFragment : BaseFragment<FragmentLoginBinding>(R.layout.fragment_login) {
+
+    private val mViewModel : LoginViewModel by viewModels()
 
     override fun onCreateBinding(
         inflater: LayoutInflater,
@@ -18,7 +23,45 @@ class LoginFragment : BaseFragment<FragmentLoginBinding>(R.layout.fragment_login
     }
 
     override fun init() {
-        //
+        requireDataBinding().viewModel = mViewModel
+
+        // 뒤로가기
+        requireDataBinding().loginCloseBtn.setOnClickListener {
+            findNavController().popBackStack()
+        }
+
+        with(mViewModel) {
+            isIdEmpty.observe(viewLifecycleOwner, Observer {
+                when (it) {
+                    true -> setIdEmptyError()
+                    false -> requireDataBinding().loginIdTextInputLayout.isErrorEnabled = false
+                }
+            })
+            isPwEmpty.observe(viewLifecycleOwner, Observer {
+                when(it) {
+                    true -> setPwEmptyError()
+                    false -> requireDataBinding().loginPwTextInputLayout.isErrorEnabled = false
+                }
+            })
+            loginErrorMsg.observe(viewLifecycleOwner, Observer {
+                showToast(getString(R.string.failure_login_msg))
+            })
+            successLogin.observe(viewLifecycleOwner, Observer {
+                successLogin()
+            })
+        }
+    }
+
+    private fun setIdEmptyError() {
+        requireDataBinding().loginIdTextInputLayout.error = getString(R.string.id_empty_error_msg)
+    }
+
+    private fun setPwEmptyError() {
+        requireDataBinding().loginPwTextInputLayout.error = getString(R.string.pw_empty_error_msg)
+    }
+
+    private fun successLogin() {
+        findNavController().popBackStack()
     }
 
 }
--- Dohyun/CloneCoding/BaeminApp/app/src/main/java/com/dohyun/baeminapp/view/login/LoginViewModel.kt
+++ Dohyun/CloneCoding/BaeminApp/app/src/main/java/com/dohyun/baeminapp/view/login/LoginViewModel.kt
@@ -1,6 +1,57 @@
 package com.dohyun.baeminapp.view.login
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.dohyun.baeminapp.base.BaseViewModel
+import com.dohyun.baeminapp.entity.UserInfo
+import com.dohyun.baeminapp.network.RetrofitBuilder
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.schedulers.Schedulers
+import io.reactivex.rxjava3.schedulers.Schedulers.io
 
 class LoginViewModel : BaseViewModel() {
+
+    val id: MutableLiveData<String> = MutableLiveData("")
+    val pw: MutableLiveData<String> = MutableLiveData("")
+
+    private val _isIdEmpty: MutableLiveData<Boolean> = MutableLiveData()
+    private val _isPwEmpty: MutableLiveData<Boolean> = MutableLiveData()
+    private val _loginErrorMsg: MutableLiveData<Unit> = MutableLiveData()
+    private val _successLogin: MutableLiveData<Unit> = MutableLiveData()
+
+    val isIdEmpty: LiveData<Boolean> get() = _isIdEmpty
+    val isPwEmpty: LiveData<Boolean> get() = _isPwEmpty
+    val loginErrorMsg: LiveData<Unit> get() = _loginErrorMsg
+    val successLogin: LiveData<Unit> get() = _successLogin
+
+    fun onLoginClick() {
+        val id = id.value.toString().trim()
+        val pw = pw.value.toString().trim()
+
+        if (id.isEmpty()) {
+            _isIdEmpty.value = true
+        } else if (pw.isEmpty()) {
+            _isPwEmpty.value = true
+        } else {
+            _isIdEmpty.value = false
+            _isPwEmpty.value = false
+            login(id, pw)
+        }
+    }
+
+    private fun login(id: String, pw: String) {
+        RetrofitBuilder.apiClient.login(UserInfo(id, pw))
+            .subscribeOn(Schedulers.computation())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe( {
+                // token 저장하기
+                Log.d("LoginViewModel", "$it")
+                _successLogin.value = Unit
+            }, {
+                // error
+                Log.e("LoginViewModel", "error : ${it.message}")
+                _loginErrorMsg.value = Unit
+            })
+    }
 }

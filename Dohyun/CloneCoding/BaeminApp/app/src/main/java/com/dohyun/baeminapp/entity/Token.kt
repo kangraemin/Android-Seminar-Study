eceef1afcbf9832f554ca47698fed2216619c893
xxria17 <xxrcy32@gmail.com>
2021-09-09 19:13:07 +0900

    사용자, 토큰 데이터 모델 생성

---
+++ Dohyun/CloneCoding/BaeminApp/app/src/main/java/com/dohyun/baeminapp/entity/Token.kt
@@ -0,0 +1,6 @@
+package com.dohyun.baeminapp.entity
+
+data class Token(
+    val refreshToken: String,
+    val accessToken: String
+)
---
+++ Dohyun/CloneCoding/BaeminApp/app/src/main/java/com/dohyun/baeminapp/entity/UserInfo.kt
@@ -0,0 +1,6 @@
+package com.dohyun.baeminapp.entity
+
+data class UserInfo(
+        val username: String,
+        val password: String
+)

f983ffeacf8f1e7e974cc4fe9e8d3ad6448516a6
anjahyun <goleftleft@gmail.com>
2021-09-09 20:24:59 +0900

    [수정]MainActivity이동 및 navigation소스 수정

--- Jaehyun/app/src/main/java/android/anjahyun/study/view/MainActivity.kt
+++ Jaehyun/app/src/main/java/android/anjahyun/study/view/MainActivity.kt
@@ -1,14 +1,13 @@
-package android.anjahyun.study
+package android.anjahyun.study.view
 
+import android.anjahyun.study.R
 import android.anjahyun.study.databinding.ActivityMainBinding
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.isVisible
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.NavigationUI
 
 class MainActivity : AppCompatActivity() {
 
@@ -25,7 +24,7 @@ class MainActivity : AppCompatActivity() {
 
         navController.addOnDestinationChangedListener { controller, destination, arguments ->
             when(destination.id) {
-                R.id.splashFragment -> findViewById<ConstraintLayout>(R.id.layout_bottom).isVisible = false
+                R.id.splashFragment, R.id.loginFragment -> findViewById<ConstraintLayout>(R.id.layout_bottom).isVisible = false
                 else -> findViewById<ConstraintLayout>(R.id.layout_bottom).isVisible = true
             }
         }

7ea5699bff06d7c630b39ca9119ef5b51e2d6e40
LeeChungWoo <skcjddn@gmail.com>
2021-09-10 10:29:15 +0900

    Fix viewbinding to databinding

--- Chungwoo/CloneBaemin/app/src/main/java/com/lcw/study/clonebaemin/feature/home/HomeFragment.kt
+++ Chungwoo/CloneBaemin/app/src/main/java/com/lcw/study/clonebaemin/feature/home/HomeFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.lcw.study.clonebaemin.R
@@ -11,14 +12,14 @@ import com.lcw.study.clonebaemin.databinding.FragmentHomeBinding
 
 
 class HomeFragment : Fragment() {
-    private var _binding: FragmentHomeBinding? = null
-    private val binding get() = _binding!!
+    private lateinit var binding: FragmentHomeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+    ): View {
+
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_home,container,false)
         return binding.root
     }
 
--- Chungwoo/CloneBaemin/app/src/main/java/com/lcw/study/clonebaemin/feature/main/MainActivity.kt
+++ Chungwoo/CloneBaemin/app/src/main/java/com/lcw/study/clonebaemin/feature/main/MainActivity.kt
@@ -2,48 +2,18 @@ package com.lcw.study.clonebaemin.feature.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.lcw.study.clonebaemin.R
 import com.lcw.study.clonebaemin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private var _binding: ActivityMainBinding? = null
-    private val binding get() = _binding!!
+    private lateinit var binding: ActivityMainBinding
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        _binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-    }
-
-    private fun init() {
-/*        binding.bottomMenu.run {
-            setOnItemSelectedListener { item ->
-                when (item.itemId) {
-                    R.id.search_tab -> {
-                        changeFragment(searchFragment)
-                    }
-                    R.id.favorite_tab -> {
-                        changeFragment(favoriteFragment)
-                    }
-                    R.id.home_tab -> {
-                        changeFragment(homeFragment)
-                    }
-                    R.id.orderlist_tab -> {
-                        changeFragment(orderListFragment)
-                    }
-                    R.id.myinfo_tab -> {
-                        changeFragment(myifoFragment)
-                    }
-                }
-                true
-
-            }
-            selectedItemId = R.id.home_tab
-
-        }*/
     }
 
 

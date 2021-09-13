a1c1b093fd32ee2a306077833703eeb0f42dc5be
LeeChungWoo <skcjddn@gmail.com>
2021-09-14 08:44:44 +0900

    Add api interface file

--- Chungwoo/CloneBaemin/app/src/main/java/com/lcw/study/clonebaemin/data/ApiService.kt
+++ Chungwoo/CloneBaemin/app/src/main/java/com/lcw/study/clonebaemin/data/ApiService.kt
@@ -7,7 +7,7 @@ import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface ApiService {
-    @POST("api/token")
+    @POST("api/token/")
     fun requestLogin(@Body requestLoginInfoData: RequestLoginInfoData):Single<LoginData>
 
 }

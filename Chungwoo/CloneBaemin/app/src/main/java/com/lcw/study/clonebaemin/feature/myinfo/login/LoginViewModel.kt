ad2239f1b748b20c8445eaa4f6c718bfafd66349
LeeChungWoo <skcjddn@gmail.com>
2021-09-14 08:49:18 +0900

    Fix LoginViewmodel Logic

--- Chungwoo/CloneBaemin/app/src/main/java/com/lcw/study/clonebaemin/feature/myinfo/login/LoginViewModel.kt
+++ Chungwoo/CloneBaemin/app/src/main/java/com/lcw/study/clonebaemin/feature/myinfo/login/LoginViewModel.kt
@@ -1,22 +1,35 @@
 package com.lcw.study.clonebaemin.feature.myinfo.login
 
 import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.lcw.study.clonebaemin.data.RetrofitClient
 import com.lcw.study.clonebaemin.data.login.LoginDatasourceImpl
+import com.lcw.study.clonebaemin.data.login.RequestLoginInfoData
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
 
-class LoginViewModel(private val loginDatasourceImpl: LoginDatasourceImpl ): ViewModel() {
+class LoginViewModel : ViewModel() {
     private val compositeDisposable = CompositeDisposable()
-    fun requestLogin(id: String, password: String) {
-        loginDatasourceImpl
-            .login(id, password)
+
+
+    private val _success: MutableLiveData<Boolean> = MutableLiveData()
+    val success: LiveData<Boolean> get() = _success
+
+
+    fun requestLogin() {
+        RetrofitClient.getService().requestLogin(RequestLoginInfoData("delivery", "dev_baemin"))
+            .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe({
-                       Log.d("LoginViewmodel","response:$it ")
+                _success.postValue(true)
+                Log.d("LoginViewmodel", "response success: $it ")
             }, {
-                Log.d("LoginViewmodel","response:$it ")
+                Log.d("LoginViewmodel", "response fail: $it ")
             })
+
     }
 
     override fun onCleared() {

4b6931bedcfed82b98efd8bd163f0b377e7ef2ab
LeeChungWoo <skcjddn@gmail.com>
2021-09-14 08:50:01 +0900

    Add databinding logic

--- Chungwoo/CloneBaemin/app/src/main/java/com/lcw/study/clonebaemin/feature/myinfo/login/LoginFragment.kt
+++ Chungwoo/CloneBaemin/app/src/main/java/com/lcw/study/clonebaemin/feature/myinfo/login/LoginFragment.kt
@@ -1,19 +1,38 @@
 package com.lcw.study.clonebaemin.feature.myinfo.login
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import com.lcw.study.clonebaemin.R
+import com.lcw.study.clonebaemin.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
+
+    private lateinit var binding : FragmentLoginBinding
+    private val loginViewModel: LoginViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+    ): View {
+
+
+
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_login, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.vm = loginViewModel
+        loginViewModel.success.observe(viewLifecycleOwner, Observer {
+            Log.d("LoginFragment","value : $it")
+        })
     }
 }

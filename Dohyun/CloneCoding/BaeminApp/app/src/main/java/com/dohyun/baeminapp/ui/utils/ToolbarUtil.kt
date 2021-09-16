a185c350a0536bd40218b849f453a005f02f3a7a
xxria17 <xxrcy32@gmail.com>
2021-09-16 15:00:39 +0900

    IdRes Annotation 추가

--- Dohyun/CloneCoding/BaeminApp/app/src/main/java/com/dohyun/baeminapp/ui/utils/ToolbarUtil.kt
+++ Dohyun/CloneCoding/BaeminApp/app/src/main/java/com/dohyun/baeminapp/ui/utils/ToolbarUtil.kt
@@ -1,13 +1,14 @@
 package com.dohyun.baeminapp.ui.utils
 
 import android.view.View
+import androidx.annotation.IdRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.FragmentActivity
 
 object ToolbarUtil {
 
-    fun initToolbar(activity: FragmentActivity?, id : Int, view: View) {
+    fun initToolbar(activity: FragmentActivity?, @IdRes id : Int, view: View) {
         val toolbar = view.findViewById<Toolbar>(id)
         val getActivity = activity as AppCompatActivity
         getActivity.setSupportActionBar(toolbar)

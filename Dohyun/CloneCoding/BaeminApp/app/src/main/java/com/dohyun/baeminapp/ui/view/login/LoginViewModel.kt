40b59d6914f085f3d4cdc51ebe66e4b496833faa
xxria17 <xxrcy32@gmail.com>
2021-09-16 15:09:19 +0900

    Login ViewModel Factory 생성

--- Dohyun/CloneCoding/BaeminApp/app/src/main/java/com/dohyun/baeminapp/ui/view/login/LoginViewModel.kt
+++ Dohyun/CloneCoding/BaeminApp/app/src/main/java/com/dohyun/baeminapp/ui/view/login/LoginViewModel.kt
@@ -54,9 +54,18 @@ class LoginViewModel : BaseViewModel() {
                 when (it.message!!.split(" ")[1]) {
                     "401" -> {
 
-                    }
-                }
-                _loginErrorMsg.value = Unit
-            })
+    }
+}
+
+class LoginViewModelFactory(
+    private val repository: LoginRepository
+): ViewModelProvider.Factory {
+
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(LoginViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return LoginViewModel(repository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class!")
     }
 }

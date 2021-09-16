a4f826b012175d6b4e9da413957949044c1d8f07
xxria17 <xxrcy32@gmail.com>
2021-09-16 15:12:22 +0900

    Token Entity 생성

--- Dohyun/CloneCoding/BaeminApp/app/src/main/java/com/dohyun/baeminapp/data/local/TokenDao.kt
+++ Dohyun/CloneCoding/BaeminApp/app/src/main/java/com/dohyun/baeminapp/data/local/TokenDao.kt
@@ -4,13 +4,13 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
 import com.dohyun.baeminapp.data.entity.Token
-import io.reactivex.Completable
-import io.reactivex.Single
+import io.reactivex.rxjava3.core.Completable
+import io.reactivex.rxjava3.core.Single
 
 @Dao
 interface TokenDao {
 
-    @Query(value = "SELECT * FROM token LIMIT 1")
+    @Query(value = "SELECT * FROM token")
     fun getToken(): Single<Token>
 
     @Insert

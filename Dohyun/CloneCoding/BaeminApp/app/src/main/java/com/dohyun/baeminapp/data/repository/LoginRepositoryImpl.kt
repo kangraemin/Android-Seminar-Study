c562c2a3f1c57fb8d20d4cb45e41f8b54693154d
xxria17 <xxrcy32@gmail.com>
2021-09-29 15:49:17 +0900

    Login Repository interface와 Impl 분리

---
+++ Dohyun/CloneCoding/BaeminApp/app/src/main/java/com/dohyun/baeminapp/data/repository/LoginRepositoryImpl.kt
@@ -0,0 +1,23 @@
+package com.dohyun.baeminapp.data.repository
+
+import com.dohyun.baeminapp.data.entity.Token
+import com.dohyun.baeminapp.data.entity.UserInfo
+import com.dohyun.baeminapp.data.local.TokenDao
+import com.dohyun.baeminapp.data.remote.ApiService
+import io.reactivex.rxjava3.core.Completable
+import io.reactivex.rxjava3.core.Single
+import javax.inject.Inject
+
+class LoginRepositoryImpl @Inject constructor(
+    private val tokenDao: TokenDao,
+    private val apiService: ApiService
+): LoginRepository {
+
+    override fun login(user: UserInfo): Single<Token> {
+        return apiService.login(user)
+    }
+
+    override fun saveTokens(token: Token): Completable {
+        return tokenDao.saveToken(token)
+    }
+}

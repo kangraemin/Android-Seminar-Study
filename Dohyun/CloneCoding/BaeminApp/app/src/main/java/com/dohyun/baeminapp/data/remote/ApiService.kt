cdb0c40d6f326c8a657b0110d387fc06d245b577
xxria17 <xxrcy32@gmail.com>
2021-09-29 15:47:31 +0900

    ApiService에 Login 관련 Api 추가

--- Dohyun/CloneCoding/BaeminApp/app/src/main/java/com/dohyun/baeminapp/data/remote/ApiService.kt
+++ Dohyun/CloneCoding/BaeminApp/app/src/main/java/com/dohyun/baeminapp/data/remote/ApiService.kt
@@ -2,6 +2,7 @@ package com.dohyun.baeminapp.data.remote
 
 import com.dohyun.baeminapp.data.entity.Token
 import com.dohyun.baeminapp.data.entity.UserInfo
+import com.dohyun.baeminapp.data.entity.VerifyToken
 import io.reactivex.rxjava3.core.Single
 import retrofit2.http.*
 
@@ -10,4 +11,9 @@ interface ApiService {
     @POST("api/token/")
     fun login(@Body user: UserInfo) : Single<Token>
 
+    @POST("api/token/refresh/")
+    fun getAccessToken(@Body refresh: String): Single<String>
+
+    @GET("test/")
+    fun testAccessToken(@Header("authorization") access: String): Single<VerifyToken>
 }

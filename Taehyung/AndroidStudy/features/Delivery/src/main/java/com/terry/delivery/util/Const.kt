32d39f13cae45949b06555fef3ca85abe56db873
Terry <choir0118@gmail.com>
2021-10-22 00:28:47 +0900

    fix : Api Key를 local property 에서 불러오도록 수정

--- Taehyung/AndroidStudy/features/Delivery/src/main/java/com/terry/delivery/util/Const.kt
+++ Taehyung/AndroidStudy/features/Delivery/src/main/java/com/terry/delivery/util/Const.kt
@@ -1,5 +1,7 @@
 package com.terry.delivery.util
 
+import com.terry.delivery.BuildConfig
+
 /*
  * Created by Taehyung Kim on 2021-10-14
  */
@@ -8,11 +10,9 @@ object Const {
     const val BASE_URL =
         "https://r5670326j8.execute-api.ap-northeast-2.amazonaws.com/delivery_server/"
 
-    private const val SEARCH_API_KEY = "iGR3hRvQ.K3wPNmJgqSMeY6CehCZmuq7Kg5Hnw3o7"
-
     fun getSearchApiHeaders(
         token: String,
-        apiKey: String = SEARCH_API_KEY
+        apiKey: String = BuildConfig.searchApiKey
     ) = hashMapOf(
         "Authorization" to "Bearer $token",
         "X-Api-Key" to apiKey
